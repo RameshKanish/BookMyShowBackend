b646e939f313d2691ba3668d41a93654f1c1f617
Ramesh Rs <rameshrs@centizen.com>
2025-01-08 11:39:49 +0530

    Add the pridction env files

--- src/main/java/com/example/BookMyShow/BookMyShowApplication.java
+++ src/main/java/com/example/BookMyShow/BookMyShowApplication.java
@@ -1,9 +1,19 @@
 package com.example.BookMyShow;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookMyShowApplication{
-	public static void main(String[] args) { SpringApplication.run(BookMyShowApplication.class, args); }
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+						.directory(".")
+								.load();
+		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+
+		SpringApplication.run(BookMyShowApplication.class, args);
+	}
 }
--- src/main/java/com/example/BookMyShow/controller/CityController.java
+++ src/main/java/com/example/BookMyShow/controller/CityController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/city")
 public class CityController {
